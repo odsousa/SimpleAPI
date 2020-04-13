7aab314a45c6adb7726960e36fb088fea6d3fd04
Odailson Sousa <odailson.sousa@outlook.com>
2020-04-13 18:22:43 -0300

    [odsousa] - broken unit test

--- src/SimpleAPI/Controllers/WeatherForecastController.cs
+++ src/SimpleAPI/Controllers/WeatherForecastController.cs
@@ -43,7 +43,7 @@ namespace SimpleAPI.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "Oda";
+            return "Odailson Sousa";
         }
     }
 }
